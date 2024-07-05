372fc9a35c5dc5264df66d01810fad67319afb22
wadsaek <wadsaek@gmail.com>
2024-07-06 00:18:51 +0300

    xwayland = true; -> xwayland.enable = true

--- nixos/home.nix
+++ nixos/home.nix
@@ -84,7 +84,7 @@
 
   wayland.windowManager.hyprland= {
     enable = true;
-    xwayland = true;
+    xwayland.enable = true;
     settings ={
       #apps for hyprland shortcuts
       "$terminal" = "alacritty";
