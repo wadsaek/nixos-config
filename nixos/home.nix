d1767648ea7abc488a6dae2b7f1263e5225d7fc4
wadsaek <wadsaek@gmail.com>
2024-07-05 23:26:20 +0300

    imported my hyprland config

--- nixos/home.nix
+++ nixos/home.nix
@@ -75,7 +75,157 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  gtk.cursorTheme.package = pkgs.rose-pine-cursor;
-  gtk.cursorTheme.name = "BreezeX Cursor";
-  
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.rose-pine-cursor;
+    name = "BreezeX Cursor";
+    size = 16;
+  };
+
+  wayland.windowManager.hyprland= {
+    settings ={
+      #apps for hyprland shortcuts
+      "$terminal" = "alacritty";
+      "$fileManager" = "alacritty -e lf";
+      "$menu" = "rofi -show drun";
+
+      #autostart
+      exec-once = "nm-applet & waybar & wpaperd & alacritty -e spotify_player";
+
+      env = [
+        "XCURSOR_SIZE,24"
+        "HYPRCURSOR_SIZE,24"
+      ];
+      general = {
+        gaps_in = 2;
+        gaps_out = 10;
+        
+        border_size = 1;
+
+        col.active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        col.inactive_border = "rgba(595959aa)";
+
+        resize_on_border = true;
+
+        allow_tearing = true;
+
+        layout = "dwindle";
+      };
+
+      decoration = {
+        rounding = 3;
+        active_opacity = 0.95;
+        inactive_opacity = 0.7;
+
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_range_power = 3;
+        col.shadow = "rgba(1a1a1aee)";
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          vibrancy = 0.1696;
+        };
+      };
+      animations = {
+        enabled = true;
+        bezier = [
+          "myBezier, 0.05, 0.9, 0.1, 1.05"
+        ];
+        animation = [
+          "windows, 1, 7, ,myBezier"
+          "windowsOut, 1,7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+      dwindle = {
+        pseudotitle = true;
+        #i probably want this?
+        preserve_split = true;
+      };
+      master = {
+        new_is_master = true;
+      };
+      misc = {
+        force_default_wallpaper = -2;
+        #DO NOT CHANGE
+        disable_hyprland_logo = false;
+      };
+      input = {
+        kb_layout = "us";
+        kb_variant = "";
+        kb_model = "";
+        kb_options = "";
+        kb_rules = "";
+        
+        follow_mouse = 1;
+        
+        sensitivity = 0; # no modification
+        touchpad = {
+          natural_scroll = true;
+        };
+      };
+      gestures = {
+        workspace_swipe = true;
+      };
+      
+      "$mainMod" = "SUPER";
+      bind = [
+        "$mainMod, Q, exec, $terminal"
+        "$mainMod, C, killactive,"
+        "$mainMod, M, exit,"
+        "$mainMod, E, exec, $fileManager"
+        "$mainMod, V, togglefloating,"
+        "$mainMod, R, exec, $menu"
+
+        #dwindle
+        "$mainMod, P, pseudo," #something weird idk i might need it sometimes... yk... like haskell
+        "$mainMod, J, togglesplit,"
+
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, up, movefocus, d"
+
+        #the special workspace
+        "$mainMod, S, togglespecialworkspace, magic"
+        "$mainMod SHIFT, S, movetoworkspace, special:magic"
+
+        "$mainMod, mouse_up, workspace, e-1"
+        "$mainMod, mouse_down, workspace, e+1"
+      ] 
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mainMod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mainMod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+      windowrulev2 = [
+        "suppressevent maximize, class:.*"
+      ];
+    };
+    extraConfig = 
+    ''
+      monitor = ,1920x1080,auto,auto
+    ''
+  }
 }
