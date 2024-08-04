c0245f416bbd4a236a55348c8c51093205771f11
wadsaek <wadsaek@gmail.com>
2024-08-04 14:44:42 +0300

    commented out network manager, turned on wpa_supplicant

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -40,14 +40,14 @@
   };
 
   networking.hostName = "Esther"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  # networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Asia/Jerusalem";
