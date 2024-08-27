85982eaf9821a75bd13a58db6390159ffc4ca621
wadsaek <wadsaek@gmail.com>
2024-08-27 23:49:26 +0300

    started working on modularity. does NOT work yet.

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -6,7 +6,7 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.home-manager
     ];
@@ -18,8 +18,7 @@
         wadsaek = import ./home.nix;
       };
     };
-#allows?
-  # Bootloader
+
   boot.loader = {
     efi.canTouchEfiVariables = true;
     grub = {
@@ -42,27 +41,18 @@
   networking.hostName = "Esther"; # Define your hostname.
   networking.wireless.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager = {
     enable = true;
     unmanaged = ["*" "except:type:wwan" "except:type:gsm"];
   };
 
-  # Set your time zone.
   time.timeZone = "Asia/Jerusalem";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_IL";
   i18n.supportedLocales = [
     "he_IL.UTF-8/UTF-8"
     "uk_UA.UTF-8/UTF-8"
     "en_IL/UTF-8"
   ];
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "he_IL.UTF-8";
     LC_IDENTIFICATION = "he_IL.UTF-8";
@@ -75,9 +65,8 @@
     LC_TIME = "he_IL.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   #services.xserver.enable = true;
+  
   services.xserver.resolutions = [{
     x = 1920;
     y = 1080;
@@ -109,29 +98,18 @@
     videoDrivers = ["nvidia"];
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
+ 
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.wadsaek = {
     isNormalUser = true;
     description = "Esther Barenboim";
@@ -147,16 +125,12 @@
   # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowBroken = true;
   nixpkgs.config.permittedInsecurePackages = [
             "electron-25.9.0"
             ];
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = (with pkgs; [
     #dependencies
     glibc
@@ -338,7 +312,7 @@
       })
   ]) ++ (with pkgs.haskellPackages; [
     tidal
-  ])++[
+  ]) ++ [
     (pkgs.writeShellScriptBin "slackwarebeatch" '' 
     echo "hello world!"
     '')
@@ -349,19 +323,11 @@
     nerdfonts
     siji
   ];
+
   environment.sessionVariables ={
     NIXOS_OZONE_WL = "1";
   };
 
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
   programs = {
   	steam = {
     	    enable = true;
@@ -382,25 +348,9 @@
       extraPackages =  [pkgs.libvdpau-va-gl ];
       enable32Bit = true;
     };
-    nvidia= {
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-      modesetting.enable = true;
-      prime = {
-        sync.enable = true;
-
-        #intel integrated
-        intelBusId = "PCI:0:2:0";
-
-        #nvidia dedicated
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
 };
 
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
     ports = [22];
@@ -412,24 +362,15 @@
       PermitRootLogin = "prohibit-password";
     };
   };
+
   virtualisation.docker = {
     enable = true;
   };
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = ["wadsaek"];
 
-  # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 22 5173 ];
   networking.firewall.allowedUDPPorts = [ 22 5173 ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+  system.stateVersion = "24.05";
 }
