448cf417f63204f7bda0e1ace7f440847cddf4f8
wadsaek <wadsaek@gmail.com>
2024-09-13 00:53:46 +0300

    edited the nushell config for nixvim support

--- modules/home/nu.nix
+++ modules/home/nu.nix
@@ -4,32 +4,18 @@
   };
 
   config = lib.mkIf config.home.nu.enable {
-    programs.nushell =
-      let 
-        nixvim = "nix run github:wadsaek/nixvim";
-      in{
+    programs.nushell = {
       enable = true;
       configFile.text = ''
         def startssh [] {
         ^ssh-agent -c | lines | first 2 | parse "setenv {name} {value};" | transpose -r | into record | load-env
         }
         
-        def nixos-boot [] {
-           cd ~/nixos/; ${nixvim} .; sudo nixos-rebuild boot --flake ../#myConfig
-        }
-
-        def nixos-switch [] {
-           cd ~/nixos/; ${nixvim} .; sudo nixos-rebuild switch --flake ../#myConfig
-        }
-
-	def nixos-rebuild-full [
-	  command: string
-	  --user (-u): string
-	  --config (-c): string
+        def nixvim [
+	  version: string
+	  ...args
 	] {
-	  cd ~/nixos/;
-	  sudo nixos-rebuild $command --flake .#$user;
-	  home-manager $command --flake .#$config
+	  nix run ("github:wadsaek/nixvim#" + $version) ...$args
 	}
 
         $env.config = {
@@ -37,16 +23,14 @@
         }
       '';
         envFile.text = ''
-        $env.EDITOR = "${nixvim}";
+        $env.EDITOR = "nvim";
       	$env.STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$env.HOME/.steam/root/compatibilitytools.d";
         $env.QT_QPA_PLATFORM = "wayland";
         '';
       shellAliases ={
         cat = "bat";
-        nvim = nixvim;
-        vi = nixvim;
-        vim = nixvim;
-        "nixvim" = nixvim;
+        vi = "nvim";
+        vim = "nvim";
         nvim-local = "${pkgs.neovim}/bin/nvim";
       };
     };
