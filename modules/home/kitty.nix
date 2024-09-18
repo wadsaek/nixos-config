9786438c292f109bd5d63344755ecb1bc8489cce
wadsaek <wadsaek@gmail.com>
2024-09-18 20:38:39 +0300

    Added font features to fira code

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -17,6 +17,7 @@
         editor = "nix run github:wadsaek/nixvim";
         notify_on_cmd_finish = "invisible 15.0";
         confirm_os_window_close = 0;
+	font_features = "FiraCodeNF-Reg +ss05 +ss03 cv30=1 +ss09";
       };
     };
   };
