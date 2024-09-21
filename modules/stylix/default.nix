36d9b21dda9ad7db7bbe3b7d99bee0a2fa4696a3
wadsaek <wadsaek@gmail.com>
2024-09-21 15:06:36 +0300

    a custom theme is a very big hastle...

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -2,24 +2,25 @@
   stylix = {
     enable = true;
 
-    base16Scheme = {
-      base00 = "242020";
-      base01 = "302828";
-      base02 = "452d2d";
-      base03 = "614343";
-      base04 = "cd8888";
-      base05 = "d4d4d4";
-      base06 = "bd18c4";
-      base0F = "e01bd0";
-      base09 = "c70d54";
-      base08 = "9e1c39";
-      base0A = "003fbd";
-      base0B = "2652ab";
-      base0C = "de7500";
-      base0D = "ba6300";
-      base0E = "2fc4a4";
-      base07 = "1c9978";
-    };
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    #{
+    #  base00 = "242020";
+    #  base01 = "302828";
+    #  base02 = "452d2d";
+    #  base03 = "614343";
+    #  base04 = "cf8888";
+    #  base05 = "d47474";
+    #  base06 = "ba3a5a";
+    #  base07 = "c70d54";
+    #  base0B = "88248f";
+    #  base0F = "ba3c9b";
+    #  base09 = "003fbd";
+    #  base0D = "2652ab";
+    #  base0A = "de7500";
+    #  base0E = "ba6300";
+    #  base08 = "2fc4a4";
+    #  base0C = "1c9978";
+    #};
 
     image = ./wallpaper.jpg;
     polarity = "dark";
