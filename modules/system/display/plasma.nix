06ad3d5d887059cf5536c5bad6e9fe52968ced63
wadsaek <wadsaek@gmail.com>
2024-10-10 22:26:17 +0300

    returned back to sddm in hopes that now hyprland will be shown (PLEASE)

---
+++ modules/system/display/plasma.nix
@@ -0,0 +1,9 @@
+{lib, config, ...}:{
+  options.display.plasma = {
+    enable = lib.mkEnableOption "plasma";
+  };
+
+  config.services.desktopManager.plasma6 = lib.mkIf config.display.plasma.enable {
+    enable = true;
+  };
+}
---
+++ modules/system/display/sddm.nix
@@ -0,0 +1,10 @@
+{lib, config, ...}:{
+  options.display.sddm = {
+    enable = lib.mkEnableOption "sddm as the display manager";
+  };
+
+  config.services.displayManager.sddm = lib.mkIf config.display.sddm.enable {
+    enable = true;
+    wayland.enable = true;
+  };
+}
