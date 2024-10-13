919f2fec4a5985f62d280c8d8f4142d3f469ca6a
wadsaek <wadsaek@gmail.com>
2024-10-13 19:05:19 +0300

    direnv

---
+++ modules/home/commandLine/direnv.nix
@@ -0,0 +1,12 @@
+{lib, config, ...}:{
+  options.home.direnv = {
+    enable = lib.mkEnableOption "direnv";
+  };
+  
+  config.programs.direnv = lib.mkIf config.home.direnv.enable {
+    enable = true;
+    nix-direnv.enable = true;
+    enableNushellIntegration = config.home.nu.enable;
+    enableZshIntegration = config.home.zsh.enable;
+  };
+}
