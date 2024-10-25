7ac3718e82479e37ad964cf011a2c62682bfbee1
wadsaek <wadsaek@gmail.com>
2024-10-25 20:22:55 +0300

    shell integration for window closing with kitty

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -22,7 +22,7 @@
 
         editor = "nix run github:wadsaek/nixvim";
         notify_on_cmd_finish = "invisible 15.0";
-        confirm_os_window_close = 0;
+        confirm_os_window_close = -1;
         font_features = "FiraCodeNF-Reg +ss05 +ss03 cv30=1 +ss09";
       };
     };
