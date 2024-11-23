8b655223cfc59353409c25330e072a03e08390b2
wadsaek <wadsaek@gmail.com>
2024-11-23 19:02:21 +0200

    nushell: envvars refactor

--- modules/home/commandLine/shells/envvars.nix
+++ modules/home/commandLine/shells/envvars.nix
@@ -1,8 +1,20 @@
-{ ... }:
+{ lib, config, ... }:
 {
   home.sessionVariables = {
     EDITOR = "nvim";
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$HOME/.steam/root/compatibilitytools.d";
     QT_QPA_PLATFORM = "wayland";
+    XCURSOR_SIZE = lib.mkForce (toString config.stylix.cursor.size);
   };
+  programs.nushell.environmentVariables = builtins.mapAttrs (
+    name: value:
+    let
+      maps = {
+        HOME = "env.HOME";
+      };
+      mapsKeys = builtins.attrNames maps;
+      mapsValues = lib.attrVals mapsKeys maps;
+    in
+    builtins.replaceStrings mapsKeys mapsValues value
+  ) config.home.sessionVariables;
 }
--- modules/home/commandLine/shells/nu/env.nu
+++
@@ -1,3 +0,0 @@
-$env.EDITOR = "nvim";
-$env.STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$env.HOME/.steam/root/compatibilitytools.d";
-$env.QT_QPA_PLATFORM = "wayland";
