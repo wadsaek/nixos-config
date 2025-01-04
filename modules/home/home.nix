b8a4ca02ea28e81faf2d7be9fc10ce3452c76845
wadsaek <wadsaek@gmail.com>
2025-01-04 14:10:34 +0200

    git signing

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -95,6 +95,12 @@
     userName = "wadsaek";
     package = pkgs.gitFull;
     enable = true;
+    extraConfig = {
+      gpg = {
+        format = "ssh";
+      };
+      user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMmFXfWeAY+Nh5z6HKW8BwMhDS0oucpFSN824RiX/7o2 wadsaek@gmail.com";
+    };
     ignores = [
       ".envrc"
       ".env"
