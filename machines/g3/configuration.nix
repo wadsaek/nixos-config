facc43b5af1463d5094c51f98d7b54b40f653f79
wadsaek <wadsaek@gmail.com>
2025-01-12 11:34:03 +0200

    reduced workload for the machine

--- machines/g3/configuration.nix
+++ machines/g3/configuration.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, ... }@inputs:
 {
   imports = [
-    ./nvidia.nix
+    # ./nvidia.nix
     ./hardware-configuration.nix
     ./../../modules/system/configuration.nix
     ./../../modules/stylix/nixos.nix
@@ -12,25 +12,11 @@
     horizontal = 1920;
   };
 
-  graphics.nvidia = {
-    enable = true;
-    prime.enable = true;
-  };
-  graphics.buses = {
-    intel = "PCI:0:2:0";
-    nvidia = "PCI:1:0:0";
-  };
-
   display = {
-    sddm.enable = true;
-    plasma.enable = true;
     hyprland.enable = true;
   };
 
   postgres.enable = true;
-  virtualisation.waydroid.enable = true;
-  services.cosmos.enable = true;
-  kmscon.enable = true;
 
   environment.etc."hyprland/monitorConfiguration".text = ''
     eDP-1, 1920x1080@60,auto, 1
