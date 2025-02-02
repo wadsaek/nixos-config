65d2c9120ab42e06b6f2867db19f8952435d86d1
wadsaek <wadsaek@gmail.com>
2025-02-02 23:00:30 +0200

    use systemd-boot

--- modules/system/essentials/bootloader.nix
+++ modules/system/essentials/bootloader.nix
@@ -7,20 +7,9 @@
 {
   boot.loader = {
     efi.canTouchEfiVariables = true;
-    grub = {
+    systemd-boot = {
       enable = true;
-      devices = [ "nodev" ];
-      efiSupport = true;
-      #useOSProber = true;
-      default = "saved";
-      theme = lib.mkForce "${
-        (pkgs.fetchFromGitHub {
-          owner = "OliveThePuffin";
-          repo = "yorha-grub-theme";
-          rev = "4d9cd37baf56c4f5510cc4ff61be278f11077c81";
-          sha256 = "sha256-XVzYDwJM7Q9DvdF4ZOqayjiYpasUeMhAWWcXtnhJ0WQ=";
-        })
-      }/yorha-${toString config.resolution.horizontal}x${toString config.resolution.vertical}";
+      editor = false;
     };
   };
 }
