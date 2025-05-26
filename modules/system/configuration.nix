50e3a4f91717023ce8fe6f72fe89534a5bb2ea34
wadsaek <wadsaek@gmail.com>
2025-05-26 15:07:39 +0300

    use extraLocales instead of supportedLocales

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -23,7 +23,7 @@
 
   time.timeZone = "Asia/Jerusalem";
   i18n.defaultLocale = "en_IL.UTF-8";
-  i18n.supportedLocales = [
+  i18n.extraLocales = [
     "he_IL.UTF-8/UTF-8"
     "uk_UA.UTF-8/UTF-8"
     "en_IL/UTF-8"
