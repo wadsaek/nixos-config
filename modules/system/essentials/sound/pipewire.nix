5787b9d0d03abc2f5eb993b1cc6402971db4fe0f
wadsaek <wadsaek@gmail.com>
2025-09-25 19:04:22 +0300

    add jack support and qpwgraph for pipewire

--- modules/system/essentials/sound/pipewire.nix
+++ modules/system/essentials/sound/pipewire.nix
@@ -1,9 +1,17 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   services.pipewire = lib.mkIf (config.audio == "pipewire") {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
+  environment.systemPackages = lib.optional (config.audio == "pipewire") pkgs.qpwgraph;
+
 }
