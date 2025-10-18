1e8c5dcea9f42b5edc90b1036cac1f68e17eaff4
wadsaek <wadsaek@gmail.com>
2025-10-18 14:47:09 +0300

    add shell starship indicator

--- modules/home/commandLine/shells/utils/starship.nix
+++ modules/home/commandLine/shells/utils/starship.nix
@@ -15,6 +15,15 @@
       enableNushellIntegration = lib.mkIf config.home.nu.enable (lib.mkDefault true);
       enableZshIntegration = lib.mkIf config.home.zsh.enable (lib.mkDefault true);
 
+      settings = {
+        format = "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$fossil_metrics$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$shell$all";
+        shell = {
+          style = "green bold";
+          format = "via [$indicator]($style) ";
+          disabled = false;
+        };
+      };
+
     };
   };
 }
