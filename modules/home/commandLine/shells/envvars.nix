3710d0356500b871df810726561194ce0eace1cb
wadsaek <wadsaek@gmail.com>
2025-10-26 08:09:56 +0200

    redo envvar declaration

--- modules/home/commandLine/shells/envvars.nix
+++ modules/home/commandLine/shells/envvars.nix
@@ -4,22 +4,18 @@
   pkgs,
   ...
 }:
+let
+  home = config.home.homeDirectory;
+in
 {
   home.sessionVariables = {
+    NOTES = "${home}/Documents/obsidian";
     EDITOR = "nvim";
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$HOME/.steam/root/compatibilitytools.d";
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "${home}/.steam/root/compatibilitytools.d";
     QT_QPA_PLATFORM = "wayland";
     XCURSOR_SIZE = lib.mkForce (toString config.stylix.cursor.size);
   };
-  programs.nushell.environmentVariables = builtins.mapAttrs (
-    name: value:
-    let
-      maps = {
-        HOME = "env.HOME";
-      };
-      mapsKeys = builtins.attrNames maps;
-      mapsValues = lib.attrVals mapsKeys maps;
-    in
-    builtins.replaceStrings mapsKeys mapsValues value
-  ) (lib.attrsets.filterAttrs (n: v: n != "XDG_CONFIG_DIRS") config.home.sessionVariables);
+  programs.nushell.environmentVariables = (
+    lib.attrsets.filterAttrs (n: v: n != "XDG_CONFIG_DIRS") config.home.sessionVariables
+  );
 }
