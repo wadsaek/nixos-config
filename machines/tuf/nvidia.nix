440e300f4c774a5a66d0b1ae3731f5be84287786
wadsaek <wadsaek@gmail.com>
2026-06-20 13:42:20 +0300

    don't use outdated nvidia modules

--- machines/tuf/nvidia.nix
+++ machines/tuf/nvidia.nix
@@ -4,10 +4,10 @@
     options nvidia_drm modeset=1 fbdev=1
   '';
   boot.initrd.availableKernelModules = [
-    "nvidia"
-    "nvidia_modeset"
-    "nvidia_uvm"
-    "nvidia_drm"
+    # "nvidia"
+    # "nvidia_modeset"
+    # "nvidia_uvm"
+    # "nvidia_drm"
   ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
